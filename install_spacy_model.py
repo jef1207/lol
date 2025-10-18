e2d51ab15526174a9977d826dda9c0810540c256
jef1207 <pultaev@bk.ru>
2025-10-18 23:13:04 +0300

    Delete install_spacy_model.py

--- install_spacy_model.py
+++
@@ -1,9 +0,0 @@
-import spacy
-
-try:
-    spacy.load("ru_core_news_sm")
-except OSError:
-    print("Downloading ru_core_news_sm...")
-    from spacy.cli import download
-    download("ru_core_news_sm")
-    print("Model downloaded!")
